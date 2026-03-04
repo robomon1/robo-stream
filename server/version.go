a230465998695d9587d4e79f5546050c2598723b
robomon1 <robomon1@gmail.com>
2026-03-04 13:09:56 -0800

    Bump client version to 0.1.3

--- server/version.go
+++ server/version.go
@@ -3,4 +3,4 @@ package main
 // Version is the server application version.
 // Keep in sync with server/wails.json info.productVersion.
 // Updated automatically by: make set-server-version VERSION=x.x.x
-var Version = "0.1.2"
+var Version = "0.1.3"
